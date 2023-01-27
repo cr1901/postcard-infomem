db61379efb62445a2a0bbac45c6bf5a7d08bc312
William D. Jones <thor0505@comcast.net>
2023-01-26 22:51:03 -0500

    Bugfixes/cleanup in generate_from_env.

--- host/src/lib.rs
+++ host/src/lib.rs
@@ -2,31 +2,40 @@ use std::error::Error;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
+use std::process::Command;
 
-use git_version::git_version;
 use postcard::to_stdvec;
 use postcard_infomem::InfoMem;
 use rustc_version::version_meta;
 use semver;
 use time::OffsetDateTime;
 
-pub fn generate_from_env() -> InfoMem {
-    fn extract_short_git_string(s: String) -> Option<String> {
-        let short_git_begin = s.find('(')? + 1;
-        let short_git_end = s[short_git_begin..].find(' ')?;
-        Some(s[short_git_begin..short_git_begin+short_git_end].to_string())
-    }
+// The short string will be fine. rustc_version is supposed to support
+// returning this, but rustc -Vv seems to return full string.
+fn extract_short_git_string(s: String) -> Option<String> {
+    let short_git_begin = s.find('(')? + 1;
+    let short_git_end = s[short_git_begin..].find(' ')?;
+    Some(s[short_git_begin..short_git_begin+short_git_end].to_string())
+}
 
+pub fn generate_from_env() -> Result<InfoMem, Box<dyn Error>> {
     let mut im = InfoMem::default();
 
     // CARGO_PKG_VERSION hardcoded while compiling this crate.
-    im.version = Some(semver::Version::parse(env!("CARGO_PKG_VERSION")).unwrap());
+    im.version = Some(semver::Version::parse(env!("CARGO_PKG_VERSION"))?);
 
-    // CARGO_PKG_VERSION comes from the parent.
+    // CARGO_PKG_VERSION comes from whatever is running this build script.
     im.user.version =
-        Some(semver::Version::parse(&std::env::var("CARGO_PKG_VERSION").unwrap()).unwrap());
-    im.user.git = Some(git_version!(args = ["--always", "--dirty"], fallback = "unknown").to_string());
-    im.user.build_date = Some(OffsetDateTime::now_local().unwrap());
+        Some(semver::Version::parse(&std::env::var("CARGO_PKG_VERSION")?)?);
+    
+    // Similar in spirit to https://github.com/fusion-engineering/rust-git-version,
+    // except done at runtime of a build-script, not compile-time of a crate.
+    im.user.git = match Command::new("git").args(["describe", "--always", "--dirty"]).output() {
+        Ok(o) if o.status.success() => Some(String::from_utf8(o.stdout).unwrap_or("unknown".to_string())),
+        _ => Some("unknown".to_string()),
+    };
+
+    im.user.build_date = Some(OffsetDateTime::now_local()?);
 
     if let Ok(rv) = version_meta() {
         im.rustc.version = Some(rv.semver);
@@ -38,11 +47,11 @@ pub fn generate_from_env() -> InfoMem {
         im.rustc.channel = Some(rv.channel);
     }
 
-    im
+    Ok(im)
 }
 
 pub fn write_info_to_file<P>(im: &InfoMem, path: P) -> Result<(), Box<dyn Error>> where P: AsRef<Path> {
-    let mut fp = File::open(path)?;
+    let mut fp = File::create(path)?;
     let buf = to_stdvec(&im)?;
     fp.write_all(&buf)?;
     
@@ -56,7 +65,7 @@ mod tests {
 
     #[test]
     fn round_trip_generate() {
-        let im = generate_from_env();
+        let im = generate_from_env().unwrap();
 
         let ser = to_stdvec(&im).unwrap();
         ser.iter().for_each(|b| {
