953a3011b7bb9325476b8c6cbbcfb9004763fc16
William D. Jones <thor0505@comcast.net>
2023-02-03 18:05:50 -0500

    Temp commit.

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,17 +1,24 @@
 #![no_std]
 #![no_main]
 
-extern crate panic_msp430;
-use msp430_rt::entry;
+use cfg_if::cfg_if;
 
-#[allow(unused)]
-use msp430g2553::interrupt;
+cfg_if! {
+    if #[cfg(feature = "msp430g2553")] {
+        extern crate panic_msp430;
+        use msp430_rt::entry;
+
+        #[allow(unused)]
+        use msp430g2553::interrupt;
+    } else if #[cfg(feature = "rp2040-hal")] {
+        extern crate panic_halt;
+    }
+}
 
 use postcard_infomem_device::include_postcard_infomem;
-
 include_postcard_infomem!(concat!(env!("OUT_DIR"), "/info.bin"));
 
-#[entry]
+#[cfg_attr(feature = "msp430g2553", entry)]
 fn main() -> ! {
     loop {}
 }
